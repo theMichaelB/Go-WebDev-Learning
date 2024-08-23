d50018b88e9e33deb76b22cad2185764334c5c44
themichaelb <michael@azured.io>
2024-08-23 05:49:06 +0000

    branch check

--- src/main.go
+++ src/main.go
@@ -6,24 +6,29 @@ import (
 	"time"
 
 	"github.com/themichaelb/Go-WebDev-Learning/internal/config"
+	"github.com/themichaelb/Go-WebDev-Learning/internal/routes"
 )
 
 func main() {
-	// const ReadHeaderTimeout = 5 * time.Second
+	const ReadHeaderTimeout = 5 * time.Second
 	const ReadTimeout = 5 * time.Second
 	const WriteTimeout = 10 * time.Second
-	// const IdleTimeout = 15 * time.Second
+	const IdleTimeout = 15 * time.Second
 
 	config.LoadConfig()
 
-	// router := routes.SetupRoutes()
+	router := routes.SetupRoutes()
 
 	log.Println("Server starting on port 8080...")
 	server := &http.Server{
-		Addr:         ":8080",
-		ReadTimeout:  ReadTimeout,
-		WriteTimeout: WriteTimeout,
+		Addr:              ":8080",
+		Handler:           router,
+		ReadHeaderTimeout: ReadHeaderTimeout,
+		ReadTimeout:       ReadTimeout,
+		WriteTimeout:      WriteTimeout,
+		IdleTimeout:       IdleTimeout,
 	}
+
 	log.Println("Server starting on port 8080...")
 	log.Fatal(server.ListenAndServe())
 }
